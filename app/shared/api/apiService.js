bf00aa2b5768c55abbd322b3e5590d7d37b82219
Hans Hardmeier <hans.hardmeier@arteries.hu>
2014-12-11 11:47:36 +0100

    Adding basic files

---
+++ app/shared/api/apiService.js
@@ -0,0 +1,10 @@
+angular.module('aas.service.api', [])
+    .service('API', ['Phonestorage', function(Phonestorage) {
+        console.log( "API service loaded" );
+
+        // the API object
+        return {
+        
+        };
+    }]
+);
